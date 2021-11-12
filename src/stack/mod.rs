1233922a90f45a2abd14c21d9390129d58de730d
Andreu Santaren Llop <andreu.santaren@gmail.com>
2021-11-12 08:16:20 +0100

    Create generic unibox that can implement any size.

--- src/stack/mod.rs
+++ src/stack/mod.rs
@@ -2,29 +2,105 @@ use core::{
     slice,
     mem,
     ptr,
-    ops::Drop,
+    ops::Drop
 };
 
 /// Generic trait for all static uniboxes.
 pub trait StaticUniBox {
+    /// Create a new UniBox instance.
+    /// 
+    /// Returns Err if the struct is bigger than N bytes.
     fn new<T: Sized>(instance: T) -> Result<Self, ()> where Self: Sized;
+    /// Get reference to stored data using a type.
     fn as_ref<T: Sized>(&self) -> &T;
-    fn as_owned<T: Sized>(&self) -> T;
+    /// Stored data length.
     fn len(&self) -> usize;
 }
 
-/// Store a struct on stack with a max size of 64 bytes.
-pub struct UniBox64 {
-    data: [u8; 64],
+/// Interface for supported array types.
+pub trait Slice {
+    fn init() -> Self;
+    fn len() -> usize;
+    fn ptr<T>(&self) -> *const T;
+    fn copy_from_byte(&mut self, src: &[u8], len: usize);
+    fn copy_from_type(&mut self, src: &Self, len: usize);
+}
+
+impl Slice for [u8; 64] {
+    fn init() -> Self {
+        [0; 64]
+    }
+
+    fn len() -> usize {
+        64
+    }
+
+    fn ptr<T>(&self) -> *const T {
+        self.as_ptr() as *const T
+    }
+
+    fn copy_from_byte(&mut self, src: &[u8], len: usize) {
+        self[0..len].clone_from_slice(src);
+    }
+
+    fn copy_from_type(&mut self, src: &Self, len: usize) {
+        self[0..len].clone_from_slice(&src[0..len]);
+    }
+}
+
+impl Slice for [u8; 128] {
+    fn init() -> Self {
+        [0; 128]
+    }
+
+    fn len() -> usize {
+        128
+    }
+
+    fn ptr<T>(&self) -> *const T {
+        self.as_ptr() as *const T
+    }
+
+    fn copy_from_byte(&mut self, src: &[u8], len: usize) {
+        self[0..len].clone_from_slice(src);
+    }
+
+    fn copy_from_type(&mut self, src: &Self, len: usize) {
+        self[0..len].clone_from_slice(&src[0..len]);
+    }
+}
+
+impl Slice for [u8; 256] {
+    fn init() -> Self {
+        [0; 256]
+    }
+
+    fn len() -> usize {
+        256
+    }
+
+    fn ptr<T>(&self) -> *const T {
+        self.as_ptr() as *const T
+    }
+
+    fn copy_from_byte(&mut self, src: &[u8], len: usize) {
+        self[0..len].clone_from_slice(src);
+    }
+
+    fn copy_from_type(&mut self, src: &Self, len: usize) {
+        self[0..len].clone_from_slice(&src[0..len]);
+    }
+}
+
+/// Generic static unibox that can implement any Slice.
+pub struct UniBoxN<S: Slice> {
+    data: S,
     len: usize,
     autodrop: fn(&Self)
 }
 
-impl StaticUniBox for UniBox64 {
-    /// Create a new UniBox instance.
-    /// 
-    /// Returns Err if the struct is bigger than 64 bytes.
-    fn new<T: Sized>(instance: T) -> Result<Self, ()> {
+impl<S: Slice> UniBoxN<S> {
+    pub fn new<T: Sized>(instance: T) -> Result<Self, ()> {
         let bytes = unsafe {
             slice::from_raw_parts(
                 (&instance as *const T) as *const u8,
@@ -35,12 +111,12 @@ impl StaticUniBox for UniBox64 {
             mem::drop(_self.as_owned::<T>());
         };
         let len = bytes.len();
-        if len > 64 {
+        if len > S::len() {
             Err(())
         }
         else {
-            let mut data = [0; 64];
-            data[0..len].clone_from_slice(bytes);
+            let mut data = S::init();
+            data.copy_from_byte(bytes, len);
             mem::forget(instance);
             Ok(
                 Self {
@@ -52,189 +128,123 @@ impl StaticUniBox for UniBox64 {
         }
     }
 
-    /// Get reference to stored data using a type.
-    fn as_ref<T: Sized>(&self) -> &T {
+    pub fn as_ref<T: Sized>(&self) -> &T {
         let len = mem::size_of::<T>();
         if len != self.len {
             panic!("Size of hosted data and requiered type are different");
         }
         unsafe {
-            mem::transmute::<&[u8; 64], &T>(&self.data)
+            mem::transmute::<&S, &T>(&self.data)
         }
     }
 
-    /// Gets ownership of the stored value.
-    /// 
-    /// After this call, the data hosted inside the UniBox won't be valid anymore.
-    fn as_owned<T: Sized>(&self) -> T {
+    pub fn as_owned<T: Sized>(&self) -> T {
         let len = mem::size_of::<T>();
-        let mut buf = [0u8; 64];
-        buf[0..len].clone_from_slice(&self.data[0..len]);
+        if len != self.len {
+            panic!("Size of hosted data and requiered type are different");
+        }
+        let mut buf = S::init();
+        buf.copy_from_type(&self.data, len);
         unsafe {
-            ptr::read(buf.as_ptr() as *const T)
+            ptr::read(buf.ptr())
         }
     }
 
-    /// Stored data length.
-    fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.len
+    }
+}
+
+impl<S: Slice> Drop for UniBoxN<S> {
+    fn drop(&mut self) {
+        println!("UniBoxN dropped");
+        (self.autodrop)(self);
+    }
+}
+
+/// Store a struct on stack with a max size of 64 bytes.
+pub struct UniBox64 {
+    unibox: UniBoxN<[u8; 64]>
+}
+
+impl StaticUniBox for UniBox64 {
+    fn new<T: Sized>(instance: T) -> Result<Self, ()> where Self: Sized {
+        Ok(
+            Self {
+                unibox: UniBoxN::new(instance)?
+            }
+        )
+    }
+
+    fn as_ref<T: Sized>(&self) -> &T {
+        self.unibox.as_ref()
+    }
+
+    fn len(&self) -> usize {
+        self.unibox.len()
     }
 }
 
 impl Drop for UniBox64 {
     fn drop(&mut self) {
         println!("UniBox64 dropped");
-        (self.autodrop)(self);
     }
 }
 
 /// Store a struct on stack with a max size of 128 bytes.
 pub struct UniBox128 {
-    data: [u8; 128],
-    len: usize,
-    autodrop: fn(&Self)
+    unibox: UniBoxN<[u8; 128]>
 }
 
 impl StaticUniBox for UniBox128 {
-    /// Create a new UniBox instance.
-    /// 
-    /// Returns Err if the struct is bigger than 128 bytes.
-    fn new<T: Sized>(instance: T) -> Result<Self, ()> {
-        let bytes = unsafe {
-            slice::from_raw_parts(
-                (&instance as *const T) as *const u8,
-                mem::size_of::<T>()
-            )
-        };
-        let autodrop = |_self: &Self| {
-            mem::drop(_self.as_owned::<T>());
-        };
-        let len = bytes.len();
-        if len > 128 {
-            Err(())
-        }
-        else {
-            let mut data = [0; 128];
-            data[0..len].clone_from_slice(bytes);
-            mem::forget(instance);
-            Ok(
-                Self {
-                    data,
-                    len,
-                    autodrop
-                }
-            )
-        }
+    fn new<T: Sized>(instance: T) -> Result<Self, ()> where Self: Sized {
+        Ok(
+            Self {
+                unibox: UniBoxN::new(instance)?
+            }
+        )
     }
 
-    /// Get reference to stored data using a type.
     fn as_ref<T: Sized>(&self) -> &T {
-        let len = mem::size_of::<T>();
-        if len != self.len {
-            panic!("Size of hosted data and requiered type are different");
-        }
-        unsafe {
-            mem::transmute::<&[u8; 128], &T>(&self.data)
-        }
+        self.unibox.as_ref()
     }
 
-    /// Gets ownership of the stored value.
-    /// 
-    /// After this call, the data hosted inside the UniBox won't be valid anymore.
-    fn as_owned<T: Sized>(&self) -> T {
-        let len = mem::size_of::<T>();
-        let mut buf = [0u8; 128];
-        buf[0..len].clone_from_slice(&self.data[0..len]);
-        unsafe {
-            ptr::read(buf.as_ptr() as *const T)
-        }
-    }
-
-    /// Stored data length.
     fn len(&self) -> usize {
-        self.len
+        self.unibox.len()
     }
 }
 
 impl Drop for UniBox128 {
     fn drop(&mut self) {
         println!("UniBox128 dropped");
-        (self.autodrop)(self);
     }
 }
-
 
 /// Store a struct on stack with a max size of 256 bytes.
 pub struct UniBox256 {
-    data: [u8; 256],
-    len: usize,
-    autodrop: fn(&Self)
+    unibox: UniBoxN<[u8; 256]>
 }
 
 impl StaticUniBox for UniBox256 {
-    /// Create a new UniBox instance.
-    /// 
-    /// Returns Err if the struct is bigger than 256 bytes.
-    fn new<T: Sized>(instance: T) -> Result<Self, ()> {
-        let bytes = unsafe {
-            slice::from_raw_parts(
-                (&instance as *const T) as *const u8,
-                mem::size_of::<T>()
-            )
-        };
-        let autodrop = |_self: &Self| {
-            mem::drop(_self.as_owned::<T>());
-        };
-        let len = bytes.len();
-        if len > 256 {
-            Err(())
-        }
-        else {
-            let mut data = [0; 256];
-            data[0..len].clone_from_slice(bytes);
-            mem::forget(instance);
-            Ok(
-                Self {
-                    data,
-                    len,
-                    autodrop
-                }
-            )
-        }
+    fn new<T: Sized>(instance: T) -> Result<Self, ()> where Self: Sized {
+        Ok(
+            Self {
+                unibox: UniBoxN::new(instance)?
+            }
+        )
     }
 
-    /// Get reference to stored data using a type.
     fn as_ref<T: Sized>(&self) -> &T {
-        let len = mem::size_of::<T>();
-        if len != self.len {
-            panic!("Size of hosted data and requiered type are different");
-        }
-        unsafe {
-            mem::transmute::<&[u8; 256], &T>(&self.data)
-        }
+        self.unibox.as_ref()
     }
 
-    /// Gets ownership of the stored value.
-    /// 
-    /// After this call, the data hosted inside the UniBox won't be valid anymore.
-    fn as_owned<T: Sized>(&self) -> T {
-        let len = mem::size_of::<T>();
-        let mut buf = [0u8; 256];
-        buf[0..len].clone_from_slice(&self.data[0..len]);
-        unsafe {
-            ptr::read(buf.as_ptr() as *const T)
-        }
-    }
-
-    /// Stored data length.
     fn len(&self) -> usize {
-        self.len
+        self.unibox.len()
     }
 }
 
 impl Drop for UniBox256 {
     fn drop(&mut self) {
         println!("UniBox256 dropped");
-        (self.autodrop)(self);
     }
 }
